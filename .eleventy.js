1d65846e919201560fa6566e0fee4d544968f3e0
Nasta Tlv <nasta.tulayeva@gmail.com>
2025-04-15 21:29:46 +0200

    Added OG meta tags

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
 module.exports = function (eleventyConfig) {
-  // Pass-through copy for static files
-  eleventyConfig.addPassthroughCopy("css");    
+  // Passthrough copy
+  eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("src/images");
 
   // Custom collections
   eleventyConfig.addCollection("now", function (collectionApi) {
-    return collectionApi.getFilteredByTag("now").reverse(); // newest first
+    return collectionApi.getFilteredByTag("now").reverse();
   });
 
   eleventyConfig.addCollection("note", function (collectionApi) {
@@ -19,9 +19,29 @@ module.exports = function (eleventyConfig) {
     return collectionApi.getFilteredByTag("case").sort((a, b) => b.date - a.date);
   });
 
+  // Custom filters
+  eleventyConfig.addFilter("excerpt", (content) => {
+    if (!content) return "";
+    return content
+      .replace(/(<([^>]+)>)/gi, "") // remove HTML tags
+      .split(" ")
+      .slice(0, 30)
+      .join(" ") + "…";
+  });
+
+  eleventyConfig.addFilter("absoluteUrl", function (url, base) {
+    try {
+      return new URL(url, base).toString();
+    } catch {
+      return url;
+    }
+  });
+
   return {
     dir: {
+      input: ".",
       includes: "_includes",
+      output: "_site",
     },
   };
 };
